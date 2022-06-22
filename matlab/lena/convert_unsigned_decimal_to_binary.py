d9ebabc4e65b174657620176626ee6f0b7ee9f0c
wrkhalil <wrkhalil@digisrv4>
2022-06-21 21:58:08 -0500

    Still fixing the shade counter testbench

--- matlab/lena/convert_unsigned_decimal_to_binary.py
+++ matlab/lena/convert_unsigned_decimal_to_binary.py
@@ -13,16 +13,16 @@ scale = 8
 
 for line in readfile:
 	n = "{0:08b}".format(int(line)).zfill(8)
-	shade_count [int(line)-1] += 1;
+	shade_count [int(line)] += 1;
 	for index in range(0, 256):
-		n_12 = "{0:08b}".format(shade_count[256-index]).zfill(12)
+		n_12 = "{0:08b}".format(shade_count[255-index]).zfill(12)
 		writefile_pixel_count_steps.write(n_12)
 	writefile_pixel_count_steps.write("\n")
 	writefile.write(n)
 	writefile.write("\n")
 
 for index in range(0, 256):
-	n_12 = "{0:08b}".format(shade_count[256-index]).zfill(12)
+	n_12 = "{0:08b}".format(shade_count[255-index]).zfill(12)
 	writefile_pixel_count.write(n_12)
 	writefile_pixel_count.write("\n")
 

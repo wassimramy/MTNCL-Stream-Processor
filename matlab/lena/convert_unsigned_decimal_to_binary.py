85aac817ee5cc43f88edeaa2db60dcc12e4d3e6e
wrkhalil <wrkhalil@digisrv4>
2022-06-21 22:05:01 -0500

    Adding all zeros in the pixel count file

--- matlab/lena/convert_unsigned_decimal_to_binary.py
+++ matlab/lena/convert_unsigned_decimal_to_binary.py
@@ -11,6 +11,11 @@ writefile_pixel_count_steps = open(sys.argv[1] + "_binary_pixel_count_steps", "w
 shade_count = [0] * 257
 scale = 8
 
+for index in range(0, 256):
+	n_12 = "{0:08b}".format(shade_count[255-index]).zfill(12)
+	writefile_pixel_count_steps.write(n_12)
+writefile_pixel_count_steps.write("\n")
+
 for line in readfile:
 	n = "{0:08b}".format(int(line)).zfill(8)
 	shade_count [int(line)] += 1;
